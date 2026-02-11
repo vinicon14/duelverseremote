8bc306a3be0d881c83452e881efb0e9999f26618
Vinícius conte Tavares marques <vinimarques12122020@gmail.com>
2026-02-11 17:07:00 -0300

    feat: arquivar torneios automaticamente após 3 dias
    - Adicionado campo archived_at na tabela tournaments
    - Criada função archive_completed_tournaments() para arquivar automaticamente
    - Criadas funções get_active_tournaments() e get_archived_tournaments()
    - Atualizadas políticas RLS para esconder torneios arquivados da listagem
    - Adicionado trigger para definir end_date quando torneio é completado
    - Torneios finalizados desaparecem automaticamente após 3 dias da interface

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7fbc4mbfnq8"
+    "revision": "0.0b9tuvofjtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
