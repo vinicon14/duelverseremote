d1b9186a8e1a2f7253e737bcda68525c3572e9f1
Vinícius conte Tavares marques <vinimarques12122020@gmail.com>
2026-02-12 05:54:57 -0300

    ajuste duelroom

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3pei5knvs4k"
+    "revision": "0.cta24dl1agg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
