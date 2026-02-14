920e93b5e6ac53ed7c3fff44db0b42afd34173b3
Vinícius conte Tavares marques <vinimarques12122020@gmail.com>
2026-02-14 18:25:18 -0300

    fix: ensure extraDeck invoke and attach buttons work

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.so0oq1jfvcs"
+    "revision": "0.4fkfumhqfko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
