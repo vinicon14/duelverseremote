aacff521bcac71b96504161e2d023ff0f2900da1
Vinícius conte Tavares marques <vinimarques12122020@gmail.com>
2026-03-10 19:39:05 -0300

    config 2

--- duelverse_app/lib/core/config/env_config.dart
+++ duelverse_app/lib/core/config/env_config.dart
@@ -10,7 +10,6 @@ class EnvConfig {
 
   static const String supabaseAnonKey = String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dHR3emV3dHF4dnBnZWZnZ2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NjY5NzQsImV4cCI6MjA3NTQ0Mjk3NH0.jhVKEu8tyid1gMnAxXZJdfrYt0a55eNpJT17hSdqtPQ',
+    defaultValue: 'YOUR_SUPABASE_ANON_KEY_HERE',
   );
 }
