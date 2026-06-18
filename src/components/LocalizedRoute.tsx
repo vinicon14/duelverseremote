a0dd8b219bf516aa0173f71d5aafcb31ac572b02
Vinícius conte Tavares marques <vinimarques12122020@gmail.com>
2026-06-18 04:28:20 -0300

    refactor(i18n): tighten LocalizedRoute types and naming

--- src/components/LocalizedRoute.tsx
+++ src/components/LocalizedRoute.tsx
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { SUPPORTED_LANGUAGES, LanguageCode } from "@/i18n/countries";
+import { SUPPORTED_LANGUAGES, type LanguageCode } from "@/i18n/countries";
 
 interface LocalizedRouteProps {
-  element: React.ComponentType;
+  component: React.ComponentType;
 }
 
-const supported = new Set<string>(SUPPORTED_LANGUAGES.map((l) => l.code));
+const supported = new Set<LanguageCode>(SUPPORTED_LANGUAGES.map((l) => l.code));
 
-export const LocalizedRoute = ({ element: Element }: LocalizedRouteProps) => {
+export const LocalizedRoute = ({ component: Component }: LocalizedRouteProps) => {
   const { lang } = useParams<{ lang: string }>();
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    if (lang && supported.has(lang) && i18n.language !== lang) {
+    if (lang && supported.has(lang as LanguageCode) && i18n.language !== lang) {
       i18n.changeLanguage(lang as LanguageCode);
     }
   }, [lang, i18n]);
 
-  return <Element />;
+  return <Component />;
 };
 
 export default LocalizedRoute;
